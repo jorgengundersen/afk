018af469d0e06153ca7008b0bc8d452ba11d3601
Jørgen Gundersen <jg@gundersenj.com>
2026-03-20 10:34:50 +0000

    feat: add Nix flake for building and development

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "afk - AFK automation tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "afk";
+            version = "0.0.0-dev";
+            src = ./.;
+            vendorHash = null;
+            meta = {
+              description = "AFK automation tool";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "afk";
+            };
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ pkgs.go ];
+          };
+        });
+    };
+}
